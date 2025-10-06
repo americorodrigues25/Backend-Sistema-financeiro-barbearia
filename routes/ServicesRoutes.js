caade5f093c4947399a1c76f27ebc1dd84c4d99c
americorodrigues25 <americo.200422@gmail..com>
2025-10-06 20:43:57 -0300

    Cria rota para filtrar produtos

--- routes/ServicesRoutes.js
+++ routes/ServicesRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const serviceController = require("../controllers/serviceController");
+const { getFilteredServices } = require("../controllers/serviceController");
 
 router.post("/", serviceController.createService);
 router.get("/total/day", serviceController.getTotalDay);
 router.get("/total/month", serviceController.getTotalMonth);
 router.get("/week", serviceController.getWeek);
 router.get("/last", serviceController.getLast);
+router.get("/filter", getFilteredServices);
+
 router.put("/:id", serviceController.updateService);
 router.delete("/:id", serviceController.deleteService);
 
