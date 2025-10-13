9ebdc4bcf9a41d3f1ed204b382d2504b4f41435b
americorodrigues25 <americo.200422@gmail..com>
2025-10-13 17:23:08 -0300

    Ajuste no cors, para oermitir frontend em modo produção

--- Server.js
+++ Server.js
@@ -19,7 +19,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", 
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Rotas
