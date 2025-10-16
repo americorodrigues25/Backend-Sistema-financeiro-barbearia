071018d7a37086fb688b3ec3ddf7507980abb42e
americorodrigues25 <americo.200422@gmail..com>
2025-10-16 12:23:47 -0300

    cria controller para trazer dados do usuario e atualizar nome

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,78 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
-// rota para trocar senha
+// trazer dados do usuario
+exports.getUserProfile = async (req, res) => {
+  try {
+    const userId = req.user.id; // vamos precisar de middleware de auth
+    const user = await User.findById(userId).select("username name email");
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Erro no servidor", error: err.message });
+  }
+};
+
+// atualizar nome do usuario
+exports.updateUserName = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { name } = req.body;
+
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, message: "O nome é obrigatório." });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Usuário não encontrado." });
+    }
+
+    user.name = name;
+    await user.save();
+
+    res.json({
+      success: true,
+      message: "Nome atualizado com sucesso!",
+      name: user.name,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Erro no servidor",
+        error: err.message,
+      });
+  }
+};
+
+// trocar senha
 exports.changePassword = async (req, res) => {
   try {
     const { email, newPassword } = req.body;
 
     if (!email || !newPassword) {
-      return res
-        .status(400)
-        .json({ success: false, message: "E-mail e nova senha são obrigatórios." });
+      return res.status(400).json({
+        success: false,
+        message: "E-mail e nova senha são obrigatórios.",
+      });
     }
 
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(404).json({ success: false, message: "Usuário não encontrado." });
+      return res
+        .status(404)
+        .json({ success: false, message: "Usuário não encontrado." });
     }
 
     const hashedPassword = await bcrypt.hash(newPassword, 10);
@@ -24,6 +81,10 @@ exports.changePassword = async (req, res) => {
 
     res.json({ success: true, message: "Senha alterada com sucesso!" });
   } catch (err) {
-    res.status(500).json({ success: false, message: "Erro no servidor.", error: err.message });
+    res.status(500).json({
+      success: false,
+      message: "Erro no servidor.",
+      error: err.message,
+    });
   }
 };
